4a935ea7562ff2b38eb36e7e2aec6cb75be53d97
Krasimir Georgiev <krasi2405@gmail.com>
2017-06-29 00:04:49 +0300

    Added rolling ball and camera.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Vector3 offset;
+    public GameObject toFollow;
+    public float stopPositionX = 95f;
+	
+	void Update () {
+        if (toFollow.transform.position.x <= stopPositionX)
+        {
+            transform.position = offset + toFollow.transform.position;
+        }
+        else
+        {
+            this.enabled = false;
+        }
+	}
+}
---
+++ Assets/Scripts/RollingBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBall : MonoBehaviour {
+
+    public Vector3 startVelocity;
+
+    private AudioSource rollingSound;
+    private Rigidbody rigidBody;
+    
+
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        rollingSound = GetComponent<AudioSource>();
+        Launch();
+    }
+	
+	void Update () {
+        
+    }
+
+    void Launch()
+    {
+        rigidBody.velocity = startVelocity;
+        rollingSound.Play();
+    }
+}
